18de3cd0bb35ca2ea8ed403887b11d20dfc54435
Dharani <dharanibalasubramanian28@gmail.com>
2024-06-15 10:31:19 +0530

    first commit

---
+++ digi_clock.py
@@ -0,0 +1,60 @@
+import tkinter as tk
+import time
+
+def update_time():
+    # Get the current time, date, and day of the week
+    current_hours_minutes = time.strftime('%I:%M')
+    current_seconds = time.strftime('%S')
+    current_am_pm = time.strftime('%p')
+    current_date = time.strftime('%B %d %Y')
+    current_day = time.strftime('%A')
+
+    # Update the labels with the current time, date, and day of the week
+    hours_minutes_label.config(text=current_hours_minutes)
+    seconds_label.config(text=current_seconds)
+    am_pm_label.config(text=current_am_pm)
+    date_label.config(text=current_date)
+    day_label.config(text=current_day)
+    
+    # Schedule the update_time function to run again after 1000 milliseconds (1 second)
+    root.after(1000, update_time)
+
+# Create the main window
+root = tk.Tk()
+root.title('Digital Clock')
+
+# Styling options
+bg_color = 'black'
+fg_color = 'green'
+font_large = ('Courier', 96, 'bold')
+font_medium = ('Courier', 48, 'bold')
+font_small = ('Courier', 24, 'bold')
+
+# Configure the main window background color
+root.configure(bg=bg_color)
+
+# Create and place the labels for hours and minutes
+hours_minutes_label = tk.Label(root, font=font_large, bg=bg_color, fg=fg_color)
+hours_minutes_label.grid(row=0, column=0, columnspan=2)
+
+# Create and place the label for seconds
+seconds_label = tk.Label(root, font=font_medium, bg=bg_color, fg=fg_color)
+seconds_label.grid(row=0, column=2)
+
+# Create and place the label for AM/PM
+am_pm_label = tk.Label(root, font=font_medium, bg=bg_color, fg=fg_color)
+am_pm_label.grid(row=0, column=3, sticky='n')
+
+# Create and place the label for the date
+date_label = tk.Label(root, font=font_small, bg=bg_color, fg=fg_color)
+date_label.grid(row=1, column=0, columnspan=4)
+
+# Create and place the label for the day of the week
+day_label = tk.Label(root, font=font_small, bg=bg_color, fg=fg_color)
+day_label.grid(row=2, column=0, columnspan=4)
+
+# Start updating the time
+update_time()
+
+# Run the Tkinter event loop
+root.mainloop()
---
+++ py1.py
@@ -0,0 +1,15 @@
+name= "John Smith"
+age= 20
+patient_type= "New"
+print("My name is",name, "I'm",age, "years old and I'm a" ,patient_type, "patient here")
+greeting= input("Tell us your guardian's name! ")
+print("Hello "+ greeting)
+#--identifier= variable
+a= 30
+print(bin(a))
+#print statement with string
+Patient_name= input("Your Name: ")
+Color= input("your favorite color: ")
+print(Patient_name+" likes "+Color)
+
+
